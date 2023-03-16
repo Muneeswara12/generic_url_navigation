007db96e735f1653f3b1e734eff65b6b576d7a0a
Muneeswara12 <kagithammunik@gmail.com>
2023-03-16 19:05:24 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def csk(request):
+    return render(request,'dhoni.html')
+def mi(request):
+    return render(request,'rohit.html')
